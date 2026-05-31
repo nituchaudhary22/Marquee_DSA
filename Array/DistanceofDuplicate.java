2563b543b5225489c51f9a40ed9dea0f11a7147f
nitu <nituchaudhary467@gmail.com>
2026-05-31 19:10:17 +0530

    New added

--- Array/DistanceofDuplicate.java
+++ Array/DistanceofDuplicate.java
@@ -2,22 +2,33 @@ package Array;
 
 public class DistanceofDuplicate {
     public static int Distance(int[] arr){
-        int maxDistance =0;
-        int distance =0;
+        int result =0;
         for(int i=0;i<arr.length;i++){
+            int search = arr[i];
             for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    distance = j-i;
-                if(distance>maxDistance){
-                    maxDistance=distance;
+                if(arr[j] == search){
+                    result = Math.max(j-i,result);
                 }
-                }
-            }          
+            }
         }
-        return maxDistance;
+                    return result;
+
+        // int maxDistance =0;
+        // int distance =0;
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[i]==arr[j]){
+        //             distance = j-i;
+        //         if(distance>maxDistance){
+        //             maxDistance=distance;
+        //         }
+        //         }
+        //     }          
+        // }
+        // return maxDistance;
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,3,3};
+        int[] arr = {1,3,3,4,5,3,3};
         System.out.println(Distance(arr));
     }
 }
