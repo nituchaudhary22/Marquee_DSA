e0a93013b451c86b95cd5905a34b3e05afade9c5
nitu <nituchaudhary467@gmail.com>
2026-06-07 22:52:44 +0530

    Arrays updated

---
+++ Array/MaxSubarrayAvg.java
@@ -0,0 +1,22 @@
+// Sliding window technique
+//given array ,find maximum subarray of size k.
+package Array;
+public class MaxSubarrayAvg{
+    public static double SubarrayAverage(int[] arr,int k){
+        int sum =0;
+        for(int i=0;i<k;i++){
+            sum += arr[i];
+        }
+        int maxAvg =sum;
+        for(int j=k;j<arr.length;j++){
+            sum = sum - arr[j-k] + arr[j];
+            maxAvg= Math.max(maxAvg, sum);
+        }
+        return (double) maxAvg/k;
+    }
+    public static void main(String[] args) {
+        int[] arr ={1,-4,-5,20,15};
+        int k =1;
+        System.out.println(SubarrayAverage(arr, k));
+    }
+}
