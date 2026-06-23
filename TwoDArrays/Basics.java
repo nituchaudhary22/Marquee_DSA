2cadb80face3058854edf52559228612983ab4de
nitu <nituchaudhary467@gmail.com>
2026-06-23 22:31:58 +0530

    2D array updated

--- TwoDArrays/Basics.java
+++ TwoDArrays/Basics.java
@@ -1,15 +1,23 @@
 package TwoDArrays;
-
+import java.util.Scanner;
 public class Basics {
-    public static void Print2Darray(int[][] arr){
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
+    public static void Print2Darray(){
+        Scanner sc =new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+        int[][] arr =new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }    
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 System.out.print(arr[i][j] + " ");
             }
         }
     }
     public static void main(String[] args) {
-        int[][] arr = {{1,2} ,{2,3,4}, {3,3,4}};
-        Print2Darray(arr);
+        Print2Darray();
     }
 }
