7c03c33b5267c9de152c668f337044947669db49
nitu <nituchaudhary467@gmail.com>
2026-06-25 10:37:33 +0530

    Function Updated

---
+++ Functions/PrimeNo.java
@@ -0,0 +1,15 @@
+package Functions;
+
+public class PrimeNo {
+    public static boolean isPrime(int n){
+        if(n <= 1) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i ==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n =16;
+        System.out.println(isPrime(n));
+    }
+}
